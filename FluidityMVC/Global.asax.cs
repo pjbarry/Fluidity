f4c06e05d7db7b168342c67dd3c1ed205940efff
PJ <pjbarry@gmail.com>
2013-09-23 12:57:00 +0100

    Basic service to check ServiceStack is working

---
+++ FluidityMVC/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using FluidityMVC.Api;
+using ServiceStack.WebHost.Endpoints;
+
+namespace FluidityMVC
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+           // WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            new FluidityTrackerAppHost().Init();
+        }
+    }
+
+    public class FluidityTrackerAppHost :AppHostBase
+    {
+        public FluidityTrackerAppHost() :base("Fluid Tracker Web Services", typeof(HelloService).Assembly) {}
+
+        public override void Configure(Funq.Container container)
+        {
+            SetConfig(new EndpointHostConfig {ServiceStackHandlerFactoryPath = "api"});
+        }
+    }
+
+
+}
